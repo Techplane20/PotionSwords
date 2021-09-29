782f4dfe7bff37bf0fbf4362a3c73160a082cb52
Techplane20 <72097686+Techplane20@users.noreply.github.com>
2021-09-29 17:05:11 -0500

    Only level 3 exists now

--- src/main/java/techplane/potion/swords/DullingTouchEnchantment.java
+++ src/main/java/techplane/potion/swords/DullingTouchEnchantment.java
@@ -23,6 +23,12 @@ public class DullingTouchEnchantment extends Enchantment {
     }
 
     @Override
+    public int getMinLevel()
+    {
+        return 3;
+    }
+
+    @Override
     public void onTargetDamaged(LivingEntity user, Entity target, int level) {
     if(target instanceof LivingEntity) {
         ((LivingEntity) target).addStatusEffect(new StatusEffectInstance(StatusEffects.MINING_FATIGUE, 20 * 2 * level, level - 1));
