c8f859ef6d043846566e358e05fa4d329d455583
Techplane20 <72097686+Techplane20@users.noreply.github.com>
2021-09-30 15:11:08 -0500

    Add files via upload

---
+++ HeliumEnchantment.java
@@ -0,0 +1,38 @@
+package techplane.potion.swords;
+
+    import net.minecraft.enchantment.Enchantment;
+    import net.minecraft.enchantment.EnchantmentTarget;
+    import net.minecraft.enchantment.Enchantments;
+    import net.minecraft.entity.Entity;
+    import net.minecraft.entity.EquipmentSlot;
+    import net.minecraft.entity.LivingEntity;
+    import net.minecraft.entity.effect.StatusEffectInstance;
+    import net.minecraft.entity.effect.StatusEffects;
+
+public class HeliumEnchantment extends Enchantment {
+
+    @Override
+    public boolean canAccept(Enchantment other) {
+        return super.canAccept(other) && other != Enchantments.SMITE && other != Enchantments.FIRE_ASPECT && other != Enchantments.SHARPNESS &&
+        other !=Enchantments.KNOCKBACK && other != Enchantments.BANE_OF_ARTHROPODS;
+    }
+    
+    @Override
+    public int getMaxLevel() {
+        return 1;
+    }
+    
+    @Override
+    public void onTargetDamaged(LivingEntity user, Entity target, int level) {
+    if(target instanceof LivingEntity) {
+        ((LivingEntity) target).addStatusEffect(new StatusEffectInstance(StatusEffects.LEVITATION, 20 * 6 * level, level - 1));
+    }
+ 
+    super.onTargetDamaged(user, target, level);
+    }
+
+    public HeliumEnchantment() {
+        super(Enchantment.Rarity.UNCOMMON, EnchantmentTarget.WEAPON, new EquipmentSlot[] {EquipmentSlot.MAINHAND});
+    }
+    
+}
